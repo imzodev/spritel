ad06fb027b1852a84353dabd127406eac7cea26c
Irving Zamora <imzodev@gmail.com>
2025-09-21 08:01:21 -0600

    feat: add users table with authentication fields and timestamps

--- prisma/migrations/20250921140041_init/migration.sql
+++ prisma/migrations/20250921140041_init/migration.sql
@@ -95,6 +95,19 @@ CREATE TABLE `Transaction` (
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- CreateTable
+CREATE TABLE `users` (
+    `id` CHAR(36) NOT NULL,
+    `name` VARCHAR(120) NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `users_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- AddForeignKey
 ALTER TABLE `PlayerInventory` ADD CONSTRAINT `PlayerInventory_playerId_fkey` FOREIGN KEY (`playerId`) REFERENCES `Player`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
